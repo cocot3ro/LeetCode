0dab4b91fd73b4a9b937c62ea4f24178007cc422
a23pablooc <a23pablooc@iessanclemente.net>
2024-04-03 00:47:33 +0200

    LC17: Accepted

---
+++ Java/1-99/LC17/src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        String digits = "22";
+
+        System.out.println(solution.letterCombinations(digits));
+    }
+}
