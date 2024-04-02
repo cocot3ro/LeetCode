31e80a47191ab245270b8b4e5f1b5a395dbe495b
a23pablooc <a23pablooc@iessanclemente.net>
2024-04-02 16:58:06 +0200

    LC390: Accepted

---
+++ Java/300-399/LC390/src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        int n = 9;
+
+        System.out.println(solution.lastRemaining(n));
+    }
+}
