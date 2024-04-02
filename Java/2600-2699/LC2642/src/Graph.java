d26f824e70ca35edc10639e67e89d27f958c6c37
a23pablooc <a23pablooc@iessanclemente.net>
2024-04-02 17:24:18 +0200

    LC2642: Accepted

--- Java/2600-2699/LC2642/src/Graph.java
+++ Java/2600-2699/LC2642/src/Graph.java
@@ -1,113 +1,52 @@
-import java.util.*;
+class Graph {
+    private final int[][] graph;
+    private final int n;
+    private final int m;
+    private final int[][] dist;
+    private final int INF = 1_000_000_000;
 
-public class Graph {
-
-    class Node {
-        int val;
-        boolean explored;
-        Map<Node, Integer> next;
-        int minorTime;
-
-        Node(int val) {
-            this.val = val;
-            this.explored = false;
-            this.next = new HashMap<>();
-            this.minorTime = Integer.MAX_VALUE;
-        }
-
-        void addNext(int n, int time) {
-            next.put(singleton.getInstance(n), time);
-        }
-
-        void reset() {
-            explored = false;
-            minorTime = Integer.MAX_VALUE;
-        }
-
-        @Override
-        public String toString() {
-            return val + "";
-        }
-    }
-
-    class Singleton {
-        Map<Integer, Node> singleton;
-
-        Singleton(int initialCapacity) {
-            this.singleton = new HashMap<>(initialCapacity);
-        }
-
-        Node getInstance(int nodeVal) {
-            if (!singleton.containsKey(nodeVal)) {
-                singleton.put(nodeVal, new Node(nodeVal));
-            }
-            return singleton.get(nodeVal);
-        }
-
-        void reset() {
-            singleton.values().forEach(Node::reset);
-        }
-    }
-
-    private final Singleton singleton;
-
-    public Graph(int initialCapacity, int[][] edges) {
-        this.singleton = new Singleton(initialCapacity);
-        for (int[] edge : edges) {
-            addEdge(edge);
-        }
-    }
-
-    public void addEdge(int[] edge) {
-        Node node = singleton.getInstance(edge[0]);
-        node.addNext(edge[1], edge[2]);
-    }
-
-    public int shortestPath(int node1, int node2) {
-        singleton.reset();
-        singleton.getInstance(node1).minorTime = 0;
-
-        if (!isReachable(node1, node2)) {
-            return -1;
-        }
-
-        return recursive(node1, node2);
-    }
-
-    private boolean isReachable(int node1, int node2) {
-        if (node1 == node2) {
-            return true;
-        }
-
-        for (Map.Entry<Node, Integer> entry : singleton.getInstance(node1).next.entrySet()) {
-            if (isReachable(entry.getKey().val, node2)) {
-                return true;
+    public Graph(int n, int[][] edges) {
+        this.n = n;
+        this.m = edges.length;
+        this.graph = new int[n][n];
+        this.dist = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                this.graph[i][j] = INF;
+                this.dist[i][j] = INF;
             }
         }
-
-        return false;
-    }
-
-    private int recursive(int node1, int node2) {
-        if (node1 == node2) {
-            return singleton.getInstance(node2).minorTime;
+        for (int i = 0; i < n; i++) {
+            this.graph[i][i] = 0;
+            this.dist[i][i] = 0;
         }
-
-        Node thisNode = singleton.getInstance(node1);
-        thisNode.explored = true;
-        int minorTime = Integer.MAX_VALUE;
-        Node minorNode = null;
-        for (Map.Entry<Node, Integer> entry : thisNode.next.entrySet()) {
-            Node nextNode = entry.getKey();
-            if (!nextNode.explored) {
-                nextNode.minorTime = Math.min(nextNode.minorTime, thisNode.minorTime + entry.getValue());
-                if (nextNode.minorTime < minorTime) {
-                    minorTime = nextNode.minorTime;
-                    minorNode = nextNode;
+        for (int i = 0; i < m; i++) {
+            int[] edge = edges[i];
+            this.graph[edge[0]][edge[1]] = edge[2];
+            this.dist[edge[0]][edge[1]] = edge[2];
+        }
+        for (int k = 0; k < n; k++) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    this.dist[i][j] = Math.min(this.dist[i][j], this.dist[i][k] + this.dist[k][j]);
                 }
             }
         }
+    }
 
-        return recursive(minorNode.val, node2);
+    public int shortestPath(int start, int end) {
+        return this.dist[start][end] == INF ? -1 : this.dist[start][end];
+    }
+
+    public void addEdge(int[] edge) {
+        this.graph[edge[0]][edge[1]] = edge[2];
+        this.dist[edge[0]][edge[1]] = edge[2];
+        for (int k = 0; k < n; k++) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    this.dist[i][j] = Math.min(this.dist[i][j], this.dist[i][k] + this.dist[k][j]);
+                }
+            }
+        }
     }
 }
