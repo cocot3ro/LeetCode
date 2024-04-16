eaa16d372938c2b31f73352cf66954ac53f84ef4
Pablo Otero Castro <pablooterocastro15@gmail.com>
2024-04-16 19:43:02 +0200

    LC645: Accepted

---
+++ Java/600-699/LC645/src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        int[] nums = {1, 2, 2, 3, 4, 5, 6, 7, 8, 9};
+
+        System.out.println(Arrays.toString(solution.findErrorNums(nums)));
+    }
+}
