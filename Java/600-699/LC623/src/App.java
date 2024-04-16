5582f61e439d9b06f0378fc43b00ddc4c1198d6a
cocot3ro <pablooterocastro15@gmail.com>
2024-04-16 15:12:50 +0200

    LC623: Accepted

---
+++ Java/600-699/LC623/src/App.java
@@ -0,0 +1,13 @@
+﻿public class App {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        
+        TreeNode root = new TreeNode(4, new TreeNode(2, new TreeNode(3), new TreeNode(1)), new TreeNode(6, new TreeNode(5), null));
+        int val = 1;
+        int depth = 2;
+        
+        solution.addOneRow(root, val, depth);
+
+        System.out.println(root);
+    }
+}
---
+++ Java/600-699/LC623/src/Solution.java
@@ -0,0 +1,24 @@
+﻿public class Solution {
+    public TreeNode addOneRow(TreeNode root, int val, int depth) {
+        if (depth == 1) {
+            return new TreeNode(val, root, null);
+        }
+        dfs(root, val, depth, 1);
+        return root;
+    }
+
+    private void dfs(TreeNode node, int val, int depth, int currentDepth) {
+        if (node == null) {
+            return;
+        }
+        
+        if (currentDepth == depth - 1) {
+            node.left = new TreeNode(val, node.left, null);
+            node.right = new TreeNode(val, null, node.right);
+            return;
+        }
+        
+        dfs(node.left, val, depth, currentDepth + 1);
+        dfs(node.right, val, depth, currentDepth + 1);
+    }
+}
