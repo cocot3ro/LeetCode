292fbb2d261e5da20ec2e6c162c45fa039ef20f4
cocot3ro <pablooterocastro15@gmail.com>
2024-09-15 22:49:04 +0200

    LC2326: Accepted

--- Java/2300-2399/LC2326/src/LC2326.java
+++ Java/2300-2399/LC2326/src/LC2326.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 
-public class App {
-    public static void main(String[] args) throws Exception {
+public class LC2326 {
+    public static void main(String[] args) {
         Solution solution = new Solution();
 
         int m = 3;
--- Java/2300-2399/LC2326/src/Solution.java
+++ Java/2300-2399/LC2326/src/Solution.java
@@ -1,33 +1,33 @@
+import java.util.Arrays;
+
 public class Solution {
 
     public int[][] spiralMatrix(int m, int n, ListNode head) {
         int[][] ans = new int[m][n];
 
-        for (int i = 0; i < ans.length; i++) {
-            for (int j = 0; j < ans[i].length; j++) {
-                ans[i][j] = -1;
-            }
+        for (int[] arr : ans) {
+            Arrays.fill(arr, -1);
         }
 
         int top = 0;
         int bot = ans.length;
         int left = 0;
-        int rigth = ans[0].length;
+        int right = ans[0].length;
 
         while (head != null) {
-            for (int i = left; i < rigth && head != null; i++) {
+            for (int i = left; i < right && head != null; i++) {
                 ans[top][i] = head.val;
                 head = head.next;
             }
             top++;
 
             for (int i = top; i < bot && head != null; i++) {
-                ans[i][rigth - 1] = head.val;
+                ans[i][right - 1] = head.val;
                 head = head.next;
             }
-            rigth--;
+            right--;
 
-            for (int i = rigth - 1; i >= left && head != null; i--) {
+            for (int i = right - 1; i >= left && head != null; i--) {
                 ans[bot - 1][i] = head.val;
                 head = head.next;
             }
