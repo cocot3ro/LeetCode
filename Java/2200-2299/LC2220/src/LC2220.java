629d6b73599d6e265a648b7534e284291d319599
cocot3ro <pablooterocastro15@gmail.com>
2024-09-15 22:52:35 +0200

    LC2220: Accepted

---
+++ Java/2200-2299/LC2220/src/LC2220.java
@@ -0,0 +1,10 @@
+public class LC2220 {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        int start = 10;
+        int goal = 7;
+
+        System.out.println(solution.minBitFlips(start, goal));
+    }
+}
