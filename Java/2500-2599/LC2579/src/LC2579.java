f2b46468a724834ebaf1c8d07b775944857097f0
cocot3ro <pablooterocastro15@gmail.com>
2025-03-05 19:29:27 +0100

    LC2579: Accepted

---
+++ Java/2500-2599/LC2579/src/LC2579.java
@@ -0,0 +1,11 @@
+public class LC2579 {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        int n = 1;
+        System.out.println(solution.coloredCells(n));
+
+        n = 2;
+        System.out.println(solution.coloredCells(n));
+    }
+}
---
+++ Java/2500-2599/LC2579/src/Solution.java
@@ -0,0 +1,5 @@
+public class Solution {
+    public long coloredCells(int n) {
+        return 1+(long)2*n*(n-1);
+    }
+}
